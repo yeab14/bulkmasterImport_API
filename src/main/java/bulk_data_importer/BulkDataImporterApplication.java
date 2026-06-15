b1bfce13def84fc627bb2c6a87b994fa3a8dcd07
Yeabsira Dereje <Yeab149@gmail.com>
2026-06-15 16:17:21 +0300

    first commit "

---
+++ src/main/java/bulk_data_importer/BulkDataImporterApplication.java
@@ -0,0 +1,13 @@
+package bulk_data_importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkDataImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkDataImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bulk_data_importer/BulkDataImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package bulk_data_importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkDataImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
